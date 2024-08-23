bff27eb11bbc09eac10e0d2763d1e382a5fc50be
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-23 23:43:19 +0530

    import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import reactDOM from "react-dom/client";
 import Home  from "./home";
-
+import Contact from "./contact";
 const root = createRoot (document.getElementById("root"))
 root.render(<h1>Home</h1>)

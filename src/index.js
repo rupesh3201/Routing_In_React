2f526d76c1ca17227f814a0f44ab14487e769096
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-23 23:53:28 +0530

    Contact

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import Home  from "./home";
 import About from "./about";
 import Contact from "./contact";
 const root = reactDOM.createRoot(document.getElementById("root"))
-root.render(<Home/>)
+root.render(<Contact/>
+
+)

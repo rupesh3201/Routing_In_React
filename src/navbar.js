b0ab2e8ebf9399d00d556c1ae68903eba5157891
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-24 18:26:05 +0530

    Home link

--- src/navbar.js
+++ src/navbar.js
@@ -2,6 +2,11 @@ import "./navbar.css"
 import react  from "react";
 function Navbar ()
 {
-    return(<div className="nav">Knox</div>)
+    return(<div className="nav">Knox
+    <div className="nav_link">
+        <a href="/">Home</a>
+    </div>
+    </div>)
+  
 }
 export default Navbar

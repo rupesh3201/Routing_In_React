1f55496adbfa226ce39ea3a639261e4868703a1c
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-24 18:31:37 +0530

    flex

--- src/navbar.js
+++ src/navbar.js
@@ -3,7 +3,7 @@ import react  from "react";
 function Navbar ()
 {
     return(<div className="nav">Knox
-    <div className="nav_link">
+    <div className="navlink">
         <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/contact">Contact</a>

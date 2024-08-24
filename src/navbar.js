1b599c5adfcc13a2cecc8601a0b52b70dab901a3
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-08-24 18:27:28 +0530

    About link

--- src/navbar.js
+++ src/navbar.js
@@ -5,6 +5,8 @@ function Navbar ()
     return(<div className="nav">Knox
     <div className="nav_link">
         <a href="/">Home</a>
+        <a href="/abput">About</a>
+        
     </div>
     </div>)
   
